90184ddb82f0c51f190a1a89a4700728ed3300f4
ty4z2008 <ty4z2008@gmail.com>
2020-04-01 14:06:32 +0800

    add telegram link

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
 	<div>
 		<router-view></router-view>
-		<a href="https://github.com/ty4z2008/frontend-nav" target="_blank" class="copyright">
-			<img src="/assets/images/icon/github-favicon.svg"/> Code of this site
-		</a>
+		<div class="footer">
+			<a href="https://github.com/ty4z2008/frontend-nav" target="_blank" class="copyright">
+				<img src="/assets/images/icon/github-favicon.svg"/> Code of this site
+			</a>
+			<a href="https://t.me/scalesystem" target="_blank" title="构建大规模分布式系统" class="copyright">
+				<img src="/assets/images/icon/telegram-favicon.png"/> Scale System
+			</a>
+		</div>
 	</div>
 </template>
 
@@ -40,19 +45,31 @@ a{
 	-webkit-transition: color .3s ease;
 	transition: color .3s ease;
 }
+.footer{
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
 .copyright{
 	color: #000;
 	font-size: 14px;
 	display: flex;
 	justify-content: center;
 	text-align: center;
+	align-items: center;
 	line-height: 40px;
+}
+.copyright+.copyright{
+	margin-left:10px;
 }
 .copyright:hover{
 	text-decoration: underline;
 }
 .copyright img{
 	margin-right: 3px;
+	width: 20px;
+	height: 20px;
+	overflow: hidden;
 }
 *{
 	background-repeat: no-repeat;
